d33a4adc82177100331b5d9ac94e61b497822685
Adimas Wisnu Wardana <wisnuwardana.adimas@gmail.com>
2019-12-05 14:15:04 +0700

    Add certificates

--- build/setup-prod.sh
+++ build/setup-prod.sh
@@ -5,5 +5,5 @@ echo "Setting up Production Connection..."
 
 # Authenticate to salesforce
 echo "Authenticating..."
-sfdx force:auth:jwt:grant --clientid $SFDC_PROD_CLIENTID --jwtkeyfile keys/server.key --username $SFDC_PROD_USER --instanceurl https://login.salesforce.com --setdefaultdevhubusername -a DevHub
+sfdx force:auth:jwt:grant --clientid $SFDC_PROD_CLIENTID --jwtkeyfile server.key --username $SFDC_PROD_USER --instanceurl https://login.salesforce.com --setdefaultdevhubusername -a DevHub
 # sfdx force:auth:jwt:grant --clientid $SFDC_PROD_CLIENTID --jwtkeyfile keys/server.key --username $SFDC_PROD_USER --setdefaultdevhubusername -a DevHub
